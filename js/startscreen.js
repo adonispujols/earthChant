77a435948a8ee60bb11770cd08a64c02e1d435e8
Adonis Pujols <adonispujols@users.noreply.github.com>
2016-08-15 22:03:26 -0400

    Updated start screen. Has more flavor to it

--- js/startscreen.js
+++ js/startscreen.js
@@ -32,30 +32,30 @@ earthChant.StartScreen.prototype = {
 		this.cursors = this.game.input.keyboard.createCursorKeys();
 		
 		// intial button (shows intro screen
-		this.initialButton = this.game.add.button(550, 520,'buttons',  		// CHANGE TO ONMOUSE CLICKED
+		this.initialButton = this.game.add.button(550, 540,'buttons',  		// CHANGE TO ONMOUSE CLICKED
 		this.showIntro,this, 2, 1, 0);
-		this.initialButton.scale.setTo(2,.5);    //rescale
+		this.initialButton.scale.setTo(2,.7);    //rescale
 		this.initialButton.anchor.set(0.5);  // sets at center
-		this.startText = this.game.add.text(this.initialButton.x, this.initialButton.y, 'Click here to Start'); //text for button
-		this.startText.anchor.set(0.5);  // sets at center
-
+		this.introText = this.game.add.text(this.initialButton.x, this.initialButton.y, 'Click here to Start', {fill:'green',font:'impact',fontSize:'45px'}); //text for button
+		this.introText.anchor.set(0.5);  // sets at center
 	},
 	
 	// shows the intro screen (add images/text here)
 	showIntro: function(){
-		// hide previous items
+		// remove previous items
 		this.initialButton.kill();
+		this.introText.kill();
 		this.logo.kill();
 		
 		// start game button (similar to initial)
-		this.startButton = this.game.add.button(500, 500,'buttons', 
+		this.startButton = this.game.add.button(550, 540,'buttons', 
 		this.startGame,this, 2, 1, 0);
-		this.startButton.scale.setTo(2,.5);
+		this.startButton.scale.setTo(2,.7);
 		this.startButton.anchor.set(0.5);  // sets at center
-		//start text
-		this.startText = this.game.add.text(this.startButton.x, this.startButton.y, 'PLAY');	// sends to start game()		
-		this.startText.anchor.set(0.5);  // sets at center
 
+		//start text
+		this.startText = this.game.add.text(this.startButton.x, this.startButton.y, 'PLAY!', {fill:'green',font:'impact',fontSize:'60px'});	// sends to start game()		
+		this.startText.anchor.set(0.5);  // sets at center
 	},
 	
 	// sends to World screen (Starts game)
